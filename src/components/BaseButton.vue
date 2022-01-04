7ec6ce45fc1aab5730ae45d98aca778ff0487d33
Fabricio Carvalho <fcarvalho1999@gmail.com>
2022-01-04 01:29:17 -0300

    Create BaseButton.vue

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <v-btn
+      @click="$emit('event-click')"
+      :to="{ name: to }"
+      :color="color"
+      class="mx-4"
+      :class="textColor"
+      >{{ text }}</v-btn
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseButton",
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+    },
+    to: {
+      type: String,
+      required: false,
+    },
+    textColor: {
+      type: String,
+      default: "white--text",
+    },
+  },
+};
+</script>
