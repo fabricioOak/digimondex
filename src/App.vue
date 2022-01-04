5e813e68c86eac15df63d65d9f999f45c13c9924
Fabricio Carvalho <fcarvalho1999@gmail.com>
2022-01-04 01:29:04 -0300

    Header changes

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <v-app>
-    <v-app-bar height="80" app color="brown darken-4" dark>
+    <v-app-bar height="80" app color="blue darken-4" dark>
       <div class="d-flex align-center">
         <v-img
-          alt="Vuetify Logo"
+          alt="Digimon logo"
           class="mr-2"
           contain
           src="https://imagensemoldes.com.br/wp-content/uploads/2021/04/Ilustracao-Digimon-PNG-1024x495.png"
@@ -15,7 +15,7 @@
       <v-spacer></v-spacer>
 
       <v-btn href="https://digimon-api.herokuapp.com/" target="_blank" text>
-        <span class="mr-2">Looking for the API?</span>
+        <span class="mr-2">Link to the API</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
